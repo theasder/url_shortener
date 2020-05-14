2e76a3edbf55939b66052ec0245387fc2e30efc3
Artem <artem108980@gmail.com>
2020-05-14 21:22:58 +0300

    added env file

--- models.py
+++ models.py
@@ -1,43 +1,47 @@
-from sqlalchemy import Table, Column, Integer, String, create_engine, MetaData
+import os
+import datetime
+
+from sqlalchemy import Table, Column, Integer, String, DateTime, create_engine, MetaData
 from sqlalchemy.engine.url import URL
 from sqlalchemy.orm import mapper
 
+
 DATABASE = {
-    'drivername': 'postgresql',
-    'host': '0.0.0.0',
-    'port': '5432',
-    'username': 'auth',
-    'password': 'auth',
-    'database': 'auth_test'
+    'drivername': os.environ['DB_TYPE'],
+    'host': os.environ['HOST'],
+    'port': os.environ['IMAGE_PORT'],
+    'username': os.environ['POSTGRES_USER'],
+    'password': os.environ['POSTGRES_PASSWORD'],
+    'database': os.environ['POSTGRES_DB']
 }
 
 engine = create_engine(URL(**DATABASE))
 metadata = MetaData()
 
 url_table = Table('URL', metadata,
-    Column('hash', String, primary_key=True),
-    Column('link', String)
+    Column('hash', String(255), primary_key=True),
+    Column('link', String(255))
 )
 
 logs_table = Table('Logs', metadata,
-    Column('hash', String, primary_key=True),
-    Column('milliseconds', Integer),
-    Column('IP', String),
-    Column('referrer', String)
+    Column('hash', String(255), primary_key=True),
+    Column('time', DateTime),
+    Column('IP', String(255)),
+    Column('referrer', String(255))
 )
 
 
 class Log(object):
-    def __init__(self, hash_, milliseconds, ip, referrer):
-        self.hash = hash_
-        self.milliseconds = milliseconds
+    def __init__(self, short_link, seconds, ip, referrer):
+        self.hash = short_link
+        self.time = datetime.datetime.fromtimestamp(seconds)
         self.ip = ip
         self.referrer = referrer
 
 
 class URL(object):
-    def __init__(self, link, hash_):
-        self.hash = hash_
+    def __init__(self, link, short_link):
+        self.hash = short_link
         self.link = link
 
 
